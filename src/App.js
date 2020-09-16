569d77a004bb49a69dcf6558d5576fb90e274fc1
Ashley Berry <ashleywberry@gmail.com>
2020-09-16 14:35:56 -0500

    basic app component

--- src/App.js
+++ src/App.js
@@ -3,23 +3,23 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  // define our initial state
+  state = {
+    location: 'St Paul',
+    adjective: 'beautiful',
+    cohort: 'Tarjan',
+    name: 'Ashley'
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <h1>{this.state.cohort} Learns State with Objects</h1>
+
+        <div>
+          Hi {this.state.name}! It's a {this.state.adjective} day in {this.state.location}
+        </div>
       </div>
     );
   }
